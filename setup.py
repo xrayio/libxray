aa133a9ba71253c5bcfc6847de5e0e958bcbbf67
Gregory Freilikhman <grisha85@users.noreply.github.com>
2017-12-23 10:56:05 +0200

    Add xraycli python packages setup

---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+
+# pip install -e .                                              # install from source
+# python setup.py sdist                                         # create dist
+# sudo pip install --no-index --find-links=./dist/ xray         # install dist
+
+setup(
+    name='xraycli',
+    version='0',
+    packages=find_packages(),
+    install_requires=[
+        'zmq',
+        'tabulate',
+    ],
+    license='MIT',
+    scripts=['cli/xraycli']
+)
